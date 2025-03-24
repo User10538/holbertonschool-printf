6c13ab2324447c2ce3cf72dd97faa078039b6506
User10538 <10538@holbertonstudents.com>
2025-03-24 00:02:25 +0000

    Upload files to save

---
+++ test/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN
+
+/**
+ * printf - function that produces output according to a format.
+ * @format: character.
+ */
+int _printf(const char *format, ...);
+
+int _putchar(char c); /**this is for putchar*/
+
+#endif /* MAIN_H */
---
+++ test/print_all.c
@@ -0,0 +1,10 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+
+int _printf(const char *format, ...)
+{
+
+}

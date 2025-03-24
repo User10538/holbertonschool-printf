51a31af6658d21a31f6d7980776ec2c27ed75933
User10538 <10538@holbertonstudents.com>
2025-03-24 00:51:12 +0000

    Upload file to save

--- test/print_all.c
+++ test/print_all.c
@@ -6,5 +6,45 @@
 
 int _printf(const char *format, ...)
 {
+	va_list args;
+	int count = 0;
+	char *str;
 
+	if (format == NULL)
+	return (-1);
+
+	va_start(args, format);
+
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if(*format == 'c')
+				count += _putchar(va_arg(args, int));
+			
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				if (str == NULL)
+				str = "(null)";
+				while (*str)
+				count += _putchar(*str++);
+			}
+			else if (*format == '%')
+			{
+				count += _putchar('%');
+			}
+			else /*invalid specifier */
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else
+			count += _putchar(*format);
+		format++;
+	}
+	va_end(args);
+	return(count);
 }

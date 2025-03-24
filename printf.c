8f3aa64b6a5cccf0fa93b39b0242be58b39bf49a
User10538 <10538@holbertonstudents.com>
2025-03-24 02:22:20 +0000

    betty fixed in printf.c

--- printf.c
+++ printf.c
@@ -2,52 +2,56 @@
 #include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * printf - function that produces output according to a format
+ * _printf - function that produces output according to a format
  * @format: characters to be printed
+ * Return: Number of char printed.
  */
 
 int _printf(const char *format, ...)
 {
-        va_list args;
-        int count = 0;
-        char *str;
+	va_list args;
+	int count = 0;
+	char *str;
 
-        if (format == NULL)
-        return (-1);
+	if (format == NULL)
+		return (-1);
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (*format)
-        {
-                if (*format == '%')
-                {
-                        format++;
-                        if(*format == 'c')
-                                count += _putchar(va_arg(args, int));
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			if (*format == 'c')
+				count += _putchar(va_arg(args, int));
 
-                        else if (*format == 's')
-                        {
-                                str = va_arg(args, char*);
-                                if (str == NULL)
-                                str = "(null)";
-                                while (*str)
-                                count += _putchar(*str++);
-                        }
-                        else if (*format == '%')
-                        {
-                                count += _putchar('%');
-                        }
-                        else /*invalid specifier */
-                        {
-                                count += _putchar('%');
-                                count += _putchar(*format);
-                        }
-                }
-                else
-                        count += _putchar(*format);
-                format++;
-        }
-        va_end(args);
-        return(count);
+			else if (*format == 's')
+			{
+				str = va_arg(args, char*);
+				if (str == NULL)
+					str = "(null)";
+				while (*str)
+					count += _putchar(*str++);
+			}
+			else if (*format == '%')
+			{
+				count += _putchar('%');
+			}
+			else /*invalid specifier */
+			{
+				count += _putchar('%');
+				count += _putchar(*format);
+			}
+		}
+		else
+		{
+			count += _putchar(*format);
+		}
+		format++;
+	}
+	va_end(args);
+	return (count);
 }

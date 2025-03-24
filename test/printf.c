dc5358c616cf0a342bf2f6786893497fbb37df66
User10538 <10538@holbertonstudents.com>
2025-03-24 03:38:23 +0000

    Fix error - Betty over line 40

--- test/printf.c
+++ test/printf.c
@@ -3,12 +3,21 @@
 #include <stdio.h>
 #include <unistd.h>
 
+int print_char(va_list args);
+int print_string(va_list args);
+int handle_specifier(char specifier, va_list args);
 
+/**
+ * _printf -  function that produces output according to a format
+ * @format: is the format
+ *
+ * Return: number of characters
+ */
 int _printf(const char *format, ...)
 {
 	va_list args;
 	int count = 0;
-	char *str;
+
 
 	if (format == NULL)
 	return (-1);
@@ -21,34 +30,67 @@ int _printf(const char *format, ...)
 		{
 			format++;
 
-			if (!*format )
+			if (!*format)
 				return (-1);
 
-			if(*format == 'c')
-				count += _putchar(va_arg(args, int));
-
-			else if (*format == 's')
-			{
-				str = va_arg(args, char*);
-				if (str == NULL)
-				str = "(null)";
-				while (*str)
-				count += _putchar(*str++);
-			}
-			else if (*format == '%')
-			{
-				count += _putchar('%');
-			}
-			else /*invalid specifier */
-			{
-				count += _putchar('%');
-				count += _putchar(*format);
-			}
+			count += handle_specifier(*format, args);
 		}
 		else
 			count += _putchar(*format);
 		format++;
 	}
 	va_end(args);
-	return(count);
+	return (count);
+}
+
+/**
+ * handle_specifier - handle the format specifiers
+ * @specifier: specifier
+ * @args: the arg list.
+ *
+ * Return: return specifiers
+ */
+int handle_specifier(char specifier, va_list args)
+{
+	if (specifier == 'c')
+		return (print_char(args));
+	if (specifier == 's')
+		return (print_string(args));
+	if (specifier == '%')
+		return (_putchar('%'));
+
+	_putchar('%');
+	return (_putchar(specifier) + 1);
+}
+
+/**
+ * print_char - to print char
+ * @args: the variable arg list
+ *
+ * Return: the number of character printed.
+ */
+int print_char(va_list args)
+{
+	return (_putchar(va_arg(args, int)));
+}
+
+/**
+ * print_string - to print the string
+ * @args: the variable arg list
+ *
+ * Return: the number of char printed.
+ */
+int print_string(va_list args)
+{
+	char *str;
+	int count = 0;
+
+	str = va_arg(args, char*);
+	if (str == NULL)
+		str = "(null)";
+
+	while (*str)
+		count += _putchar(*str++);
+
+	return (count);
 }
